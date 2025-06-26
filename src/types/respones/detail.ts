ecf95b0de15755b07a8d66947f06c1f38fd923b7
NTTai - PC09243 <nguyentantaivithanh@gmail.com>
2025-06-26 14:53:11 +0700

    fix jobs

--- src/types/respones/detail.ts
+++ src/types/respones/detail.ts
@@ -17,7 +17,7 @@ export namespace ResponseDetail {
     employerRequtation: number;
     title: string;
     description: string;
-    countApply: number;
+    countApplies: number;
     budget: number;
     durationHours: number;
     postedAt: string;
